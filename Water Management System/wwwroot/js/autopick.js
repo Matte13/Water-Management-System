9bd140a081d886cda8fc685df8800528bce9a0b0
Matteo Schirinzi <47254737+Matte13@users.noreply.github.com>
2023-10-04 12:51:44 +0200

    Add files via upload

---
+++ Water Management System/wwwroot/js/autopick.js
@@ -0,0 +1,29 @@
+﻿$("#compid").change(runme());
+
+
+function runme() {
+
+    $.ajax({
+        url: '/WaterManagment/GetCrop',
+        type: 'GET',
+        async: true,
+        cache: false,
+
+
+        data: { id: $("#compid").val() },
+
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var s = '';
+
+            for (var i = 0; i < result.length; i++) {
+
+                s += '<option value="' + result[i].id + '">' +
+                    result[i].name + '</option>';
+            }
+            $("#CropDropdown").html(s);
+
+        }
+    });
+}
